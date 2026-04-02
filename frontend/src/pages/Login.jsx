0c2cd384e72bf9137d49bc3862dcee9254ebd709
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-02 13:57:06 +0530

    update styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
         @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Mono:wght@300;400;500&display=swap');
 
         .login-root {
-          min-height: 100vh;
+          min-height: 99vh;
           margin-top: -7rem;
           background: #080a0f;
           display: flex;
